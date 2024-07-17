30d880c8f8a220846013bb8d63906473fa2cf7f0
Olgasyla <olgasyla@gmail.com>
2024-07-17 12:12:07 +0200

    Add method return Order with OrderStatus

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,10 +1,15 @@
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
     private OrderRepo orderRepo = new OrderMapRepo();
+    private List<Order> orders = new ArrayList<>();
+    private OrderStatus OrderStatus;
 
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
@@ -17,8 +22,14 @@ public class ShopService {
             products.add(productToOrder);
         }
 
-        Order newOrder = new Order(UUID.randomUUID().toString(), products);
+        Order newOrder = new Order(UUID.randomUUID().toString(), products,OrderStatus);
 
         return orderRepo.addOrder(newOrder);
+    }
+    public void getOrderByStatus(OrderStatus status) {
+        orders.stream()
+                .filter(orders -> orders.getStatus().equals(status))
+                .collect(Collectors.toList());
+
     }
 }
